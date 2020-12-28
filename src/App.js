4bc7481b331f195f9f21f33cc193a6980eeefdec
Uriel Awe-obe <urielaweobe@gmail.com>
2020-12-28 20:00:40 +0100

    finishing sign in handleSubmit method

--- src/App.js
+++ src/App.js
@@ -24,15 +24,13 @@ class App extends React.Component {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
 
-        userRef.onSnapshot(snapShot => {
+        userRef.onSnapshot((snapShot) => {
           this.setState({
             currentUser: {
               id: snapShot.id,
               ...snapShot.data(),
             },
           });
-
-          console.log(this.state);
         });
       } else {
         this.setState({ currentUser: userAuth });
